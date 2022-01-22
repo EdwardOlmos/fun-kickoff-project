37a5c8c1cfc8dd98166369ad43d4e76e619468ec
Edward Olmos <edwardolmos3@gmail.com>
2022-01-21 17:37:29 -0800

    added Profile Model

---
+++ server/models/Profile.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+
+const profileSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  gender: {
+    type: ["male", "female"]
+  },
+  birthDate: Date,
+  phoneNumber: {
+    type: String,
+    required: true
+  },
+  address: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  photoUrl: {
+    type: String,
+    required: true
+  },
+  payment: {
+    type: String,
+    required: true
+  },
+  availability: [
+    {
+      date: Date,
+      startTime: Date,
+      endTime: Date
+    }
+  ]
+});
+
+module.exports = Profile = mongoose.model("profile", profileSchema);
